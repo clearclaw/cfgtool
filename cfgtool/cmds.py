8850642af7a5a5581fae41d91b3cfaf36ce59d05
J C Lawrence <claw@kanga.nu>
2015-07-20 22:01:51 -0700

    notroot

--- cfgtool/cmds.py
+++ cfgtool/cmds.py
@@ -6,7 +6,7 @@ from cfgtool.cmdbase import implementation
 
 LOG = logging.getLogger (__name__)
 OPTIONS = ["help", "workdir", "moduledir", "beliefdir", "nobackup", "debug",
-               "force", "nocolour", "quiet", "verbose", "noroot",]
+               "force", "nocolour", "quiet", "verbose", "notroot",]
 
 @app_main.subcommand (
   name = "check",
